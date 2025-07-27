2f012f3a8dee44679ce72bb69447fa7c7e414222
Holger Blasum <holger.blasum@urstrom.de>
2025-07-27 09:09:01 +0000

    add daily script

---
+++ ui/daily.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+BACKLOG=5
+if [ "$#" -gt 0 ]; then
+    SYSTEM="$1"
+fi
+if [ "$#" -gt 1 ]; then
+    BACKLOG="$2"
+fi
+export ANALYSIS_BEGIN=`date -d "-$BACKLOG days" "+%Y-%m-%d"`
+export ANALYSIS_END=`date -d "yesterday" "+%Y-%m-%d"`
+(cd /home/sunshine/pv-data-parser && export PYTHONPATH=. && python3 ui/db_import_from_file_via_pickle_urstrom_all.py $ANALYSIS_BEGIN $ANALYSIS_END $SYSTEM) > ~/pv-data-parser/log/cron-sunshine-$ANALYSIS_END-parse 2>&1
