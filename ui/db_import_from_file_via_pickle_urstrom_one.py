dd3fbb2f19eb8f946401149f442441d350399421
Holger Blasum <holger.blasum@urstrom.de>
2025-08-24 15:54:42 +0000

    add new systems

--- ui/db_import_from_file_via_pickle_urstrom_one.py
+++ ui/db_import_from_file_via_pickle_urstrom_one.py
@@ -9,7 +9,7 @@ def db_import_from_file_via_pickle_urstrom_one(pv_system_id, date_begin, date_en
         return
     parse_format = "js"
     # CSV data
-    if pv_system_id in (13, 14, 16, 17, 18, 20, 21, 22):
+    if pv_system_id in (13, 14, 16, 17, 18, 19, 20, 21, 22, 31):
         parse_format = "csv"
     # CSV data, format 1.0, ISO-8859-1
     # HBL if pv_system == 16 or pv_system == 17:
