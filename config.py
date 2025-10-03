46978a857ce1dda34d803cfce7bcd8ac6e7ce8c6
Holger Blasum <holger.blasum@urstrom.de>
2025-10-03 15:59:43 +0200

    Add empty list for tracker_mask to defaults

--- config.py
+++ config.py
@@ -1,8 +1,9 @@
 path_base = '/home/xxx'
 path_data_raw = path_base + '/data_raw'
 path_data_processed = path_base + '/data_processed'
-database_name = "sunshinedb"
+database_name = "sunshine"
 database_user = "xxx"
 database_password = "xxx"
 database_port = 5432 # postgresql
 database_host = "192.168.1.1"
+tracker_mask = []
