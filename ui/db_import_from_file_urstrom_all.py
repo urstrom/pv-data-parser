921edcf0fcdbd763130328fa8f56b76408c2ea3f
Holger Blasum <holger.blasum@urstrom.de>
2026-05-25 17:32:55 +0200

    Add path argument

--- ui/db_import_from_file_urstrom_all.py
+++ ui/db_import_from_file_urstrom_all.py
@@ -1,11 +1,12 @@
-import sys
+import sys, config
 import db_import_from_file, urstrom
 
 def db_import_from_file_urstrom_all(date_begin, date_end):
     for pv_system_id in range(1, urstrom.get_number_systems() + 1):
         parse_function = urstrom.get_parse_function(pv_system_id, date_begin, date_end)
         if parse_function is not None:
-            db_import_from_file.db_import_from_file(pv_system_id, date_begin, date_end, parse_function)
+            db_import_from_file.db_import_from_file(pv_system_id, f"{config.path_data_raw}{pv_system_id:02}",
+                date_begin, date_end, parse_function)
 
 if __name__ == "__main__":
     db_import_from_file_urstrom_all(sys.argv[1], sys.argv[2])
